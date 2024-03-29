3bbae0b3d2ff28a84bb7757bedcd67516f21ef5e
antilopa <mikbas31@gmail.com>
2024-03-29 14:39:56 +0200

    bad bad bad

--- core/src/main/kotlin/warped/realms/system/update/mapper/ServerDismapperSystem.kt
+++ core/src/main/kotlin/warped/realms/system/update/mapper/ServerDismapperSystem.kt
@@ -2,15 +2,20 @@ package warped.realms.system.update.mapper
 
 import Update
 import System
+import server_connector.ServerConnector
+import warped.realms.entity.mapper.EntityMapper
 
 @System
 @Update(100)
 class ServerDismapperSystem {
+    private val entityMappers = mutableListOf<EntityMapper>()
+    lateinit var serverConnector: ServerConnector
     fun Update(delta: Float) {
-
+        val p = arrayOf(serverConnector.serverRequest.serverQueue.pop().toByte())
+        entityMappers.forEach { it.dismapEntity(p.toByteArray()) }
     }
 
     fun Dispose() {
-
+        entityMappers.clear()
     }
 }
--- core/src/main/kotlin/warped/realms/system/update/mapper/ServerMapperSystem.kt
+++ core/src/main/kotlin/warped/realms/system/update/mapper/ServerMapperSystem.kt
@@ -2,16 +2,19 @@ package warped.realms.system.update.mapper
 
 import Update
 import System
+import warped.realms.component.ImageComponent
+import warped.realms.entity.mapper.EntityMapper
 
 @System
 @Update(-2)
 class ServerMapperSystem {
+    private val entityMappers = mutableListOf<EntityMapper>()
 
     fun Update(deltaTime: Float) {
-
+        entityMappers.forEach { it.Update() }
     }
 
     fun Dispose() {
-
+        entityMappers.clear()
     }
 }
